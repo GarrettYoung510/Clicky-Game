5fea63c22ff671f6311ec87f061d7f00c1c5821b
Garrett Young <garrettyoung510@gmail.com>
2019-07-30 13:36:33 -0700

    built out app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,6 @@
-import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-import clickBox from './components/clickBox';
+import React from 'react';
+import Game from "./containers/Game";
 
-const getRandomSpots = () => {
-  // add boxes to random spots on the grid
-}
+const App = () => <Game />
 
-class App extends Component {
-  state = {
-    clickBox: getRandomSpots()
-  }
-
-
-render() {
-  return (
-    <div className="container">
-      <div className="row">
-        <h1>Hello World</h1>
-        <clickBox/>
-        {/* 3 rows */}
-        {/* 4 containers per row squares */}
-      </div>
-    </div>
-  );
-}
-}
 export default App;
